b17e9aad11e7174bd0e0e52cd6b309abf50fc16e
Jagadesh Boopathi <jagansindia@yahoo.com>
2018-03-29 17:23:39 +0530

    Update config.py

--- config.py
+++ config.py
@@ -4,9 +4,9 @@ import os
 class Config(object):
     DEBUG = False
     DB_NAME = "heroku_dj9l47s3"
-    DB_HOST = "mongodb://ds113799.mlab.com:13799"
-    DB_USERNAME = "heroku_dj9l47s3"
-    DB_PASSWORD = "heroku_dj9l47s3"
+    DB_HOST = "mongodb://heroku_dj9l47s3:cadjq6j0vecegujc7jal00e7qk@ds113799.mlab.com:13799/heroku_dj9l47s3"
+    DB_USERNAME = ""
+    DB_PASSWORD = ""
     # Web Server details
     WEB_SERVER_PORT = 8001
 
